b9a3a1128b6ae6606e6d12d7937e81951ed52282
curegit <37978051+curegit@users.noreply.github.com>
2021-01-13 10:41:31 +0900

    Fix

--- standard/convert.py
+++ standard/convert.py
@@ -26,7 +26,7 @@ stride = patch - pad * 2
 
 img = cv2.imread(sys.argv[2], -1)[:,:,[2,1,0]]
 img1 = (img.transpose(2, 0, 1) / 65535).astype("float32")
-img1 = np.pad(img1, [(0, 0), (64, 64), (64, 64)], "edge")
+img1 = np.pad(img1, [(0, 0), (pad, pad), (pad, pad)], "edge")
 h, w = img1.shape[1:3]
 
 out = np.ones((3, h, w), dtype="float32")
@@ -41,8 +41,8 @@ for x in range(0, w, stride):
 		v = Variable(model.xp.array(im))
 		t = model(v)[0]
 		t.to_cpu()
-		out[:,y1:y1+stride,x1:x1+stride] = t.data[0]
+		out[:,y1:y1+stride,x1:x1+stride] = t.data
 
 print(out.shape)
 img2 = np.clip(np.rint(out * 65535), 0, 65535).astype(np.uint16).transpose(1, 2, 0)[:,:,[2,1,0]]
-cv2.imwrite(sys.argv[2] + f"-dehalf{sys.argv[1]}.png", img2)
+cv2.imwrite(f"{sys.argv[3]}.png", img2)

e308ee81c17d96ee03af238d7f940debe4fca2b9
curegit <37978051+curegit@users.noreply.github.com>
2024-03-06 18:53:36 +0900

    Update cand

--- descreen/networks/__init__.py
+++ descreen/networks/__init__.py
@@ -41,8 +41,8 @@ class AbsModule(ABC, Module):
 
         #self.patch_slices(height, width, out_patch_size, p)
 
-        qh = self.multiple_of - self.patch_slices_remainder(height, out_patch_size)
-        qw = self.multiple_of - self.patch_slices_remainder(width, out_patch_size)
+        qh = input_patch_size - self.patch_slices_remainder(height + 2 * p, input_patch_size, p)
+        qw = input_patch_size - self.patch_slices_remainder(width + 2 * p, input_patch_size, p)
         y = pad(x, (*([(0, 0)] * (x.ndim - 2)), (p, qh + p), (p, qw + p)), mode=pad_mode, **kwargs)
         h_crop = slice(p, height + p)
         w_crop = slice(p, width + p)
@@ -51,16 +51,23 @@ class AbsModule(ABC, Module):
     def patch_slices(self, height: int, width: int, output_patch_size: int, padding: int):
         for h_start, h_stop in range_chunks(height, output_patch_size):
             for w_start, w_stop in range_chunks(width, output_patch_size):
-                h_pad = self.required_padding(h_stop - h_start)
-                w_pad = self.required_padding(w_stop - w_start)
+                h_pad = padding #self.required_padding(h_stop - h_start)
+                w_pad = padding #self.required_padding(w_stop - w_start)
+                assert h_stop - h_start == w_stop - w_start == output_patch_size
                 h_slice = slice(h_start - h_pad + padding, h_stop + h_pad + padding)
                 w_slice = slice(w_start - w_pad + padding, w_stop + w_pad + padding)
                 h_dest_slice = slice(h_start + padding, h_stop + padding)
                 w_dest_slice = slice(w_start + padding, w_stop + padding)
                 yield (h_slice, w_slice), (h_dest_slice, w_dest_slice)
 
-    def patch_slices_remainder(self, length: int, output_patch_size: int):
-        input_size = self.input_size(output_patch_size)
+    def patch_slices_remainder(self, length: int, input_patch_size: int, padding: int):
+        cur = 0
+        while length - cur <= input_patch_size:
+            cur += input_patch_size - padding * 2
+        return input_patch_size % (length - cur)
+
+
+        #input_size = self.input_size(output_patch_size)
         for start, stop in range_chunks(length, output_patch_size):
             pad = self.required_padding(stop - start)
             size = stop - start + 2 * pad
